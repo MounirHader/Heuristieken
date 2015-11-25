9e39e40ce7d36f7813debae918dec765e5ddcfdb
maartenalblas <maarten_alblas@hotmaill.com>
2015-11-25 15:35:14 +0100

    kleinprobleem split up in preparation and schedule_maker

--- schedule_maker.py
+++ schedule_maker.py
@@ -1,42 +1,3 @@
-class Student(object):
-    """
-    A Student represents a certain student
-
-    """
-    def __init__(self, student_id, courses):
-        """
-        Initializes a student with courses
-
-        """
-        self.student_id = student_id
-        self.courses = courses
-
-    def numberCourses(self):
-        """
-        returns number of courses
-
-        """
-        return len(self.courses)
-
-class Course(object):
-    """
-    A Course represents a certain course.
-    """
-    def __init__(self, name, students):
-        """
-        Initializes a course with students
-
-        """
-        self.name = name
-        self.students = students
-
-    def numberStudents(self):
-        """
-        returns number of students
-
-        """
-        return len(self.students)
-
 
 
 class TimeSlot(object):
@@ -138,58 +99,19 @@ class ScheduleStudent(object):
         self.time_slots = []
         self.type = "student"
 
-
-def scheduleMaker():
+def main():
     '''
-    Extracts data from original CSV file (studenten_roostering).
+    Creates a schedule for room and students.
 
-    Outputs lists: student_list and course_list, which are lists of objects
+    Outputs: schedule_room object and schedule_student_list which is a list of objects
     '''
-    import csv
-    import random
 
-    # open csv file and reads into csv-file
-    f = open('studenten_roostering.csv')
-    csv_file = csv.reader(f)
+    import preparation
 
-    # empty list for student and course objects
-    student_list = []
-    course_list = []
-    student_infos = []
-
-    # extracts the id and courses of every student
-    for csv_line in csv_file:
-        student_infos.append(csv_line)
-
-    for student_info in student_infos[1:10]:
-        student_id = student_info[2]
-        courses_student = []
-        for student_course in student_info[3:]:
-            if student_course != "":
-                courses_student.append(student_course)
-
-
-        # [course for course in ["vak1", "vak2", "", ""] if course]
-        # create new student object and adds it to list
-        student = Student(student_id, courses_student)
-
-        student_list.append(student)
-
-        # loops over courses of the student
-        for course_student in courses_student:
-            course_exists = False
-            # checks if course already exists
-            for course in course_list:
-                if course.name == course_student:
-                    course.students.append(student_id)
-                    course_exists = True
-            if course_exists == False:
-                if course_student != '':
-                    # creates new course object w
-                    students = []
-                    course = Course(course_student, students)
-                    course_list.append(course)
-                    course.students.append(student_id)
+    # creates a list of student and course objects in preparation file
+    lists = preparation.main()
+    student_list = lists[0]
+    course_list = lists[1]
 
     # creates one schedule_room object
     schedule_room = ScheduleRoom('A5')
@@ -245,9 +167,11 @@ def scheduleMaker():
         # appends time_slot to time_slot_list
         time_slot_list.append(time_slot)
 
-        # # test
-        # test = [time_slot.name[:2],time_slot.day, time_slot.hour]
-        # print_list.append(test)
-        # print print_list
+        # test
+        test = [time_slot.name[:2],time_slot.day, time_slot.hour]
+        print_list.append(test)
+        print print_list
 
     return [schedule_room, schedule_student_list]
+
+main()
