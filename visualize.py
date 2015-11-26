0dc9896192d1972212aa31ba34b62f713e15dc53
MounirHader <2527137@student.vu.nl>
2015-11-26 11:55:26 +0100

    visualisatie "netjes"

--- visualize.py
+++ visualize.py
@@ -1,58 +1,15 @@
-import kleinprobleem, sys, pygame
-
-# run scheduleMaker
-schedule = kleinprobleem.scheduleMaker()
-
-schedule_room = schedule[0]
-schedule_student_list = schedule[1]
-
-
-
-#
-#
-#
-# print "Dit is zijn de timeslots voor het klaslokaal"
-# for time_slot in schedule_room.time_slots:
-#   print "course: "
-#   print time_slot.name
-#   print "students: "
-#   print time_slot.students
-#   print "day: "
-#   print time_slot.day
-#   print "hour: "
-#   print time_slot.hour
-#
-# print "!!! Nu komen de timeslots  van de studenten !!!"
-# for schedule_student in schedule_student_list:
-#   print "Timeslots van: " + schedule_student.student_id
-#   for time_slot in schedule_student.time_slots:
-#       print "course: "
-#       print time_slot.name
-#       print "students: "
-#       print time_slot.students
-#       print "day: "
-#       print time_slot.day
-#       print "hour: "
-#       print time_slot.hour
-
-# monday
-# if (time_slot.day == 0 && time_slot.hour == 0):
-
+import schedule_maker, sys, pygame
 
 def visualize(schedule):
     pygame.init()
 
-    black = (0,0,0)
-    red = (255,0,0)
-    white = (255,255,255)
-    gekkigheid = (255, 240, 240)
-    grey = (158,158,158)
-
-    display_x = 500
-    display_y = 500
+    background_color = (240,240,240)
+    timeslot_color = (99,99,99)
+    text_color = (0,0,0)
+    display_x = 575
+    display_y = 530
 
     gameDisplay = pygame.display.set_mode((display_x, display_y))
-    pygame.display.set_caption('Gimmah')
 
     gameExit = False
 
@@ -61,29 +18,49 @@ def visualize(schedule):
             if event.type == pygame.QUIT:
                 gameExit = True
 
-        gameDisplay.fill(gekkigheid)
-        screen = gameDisplay.fill(grey, rect=[857,100,350,600])
+        # initialize window
+        gameDisplay.fill(background_color)
+        # screen = gameDisplay.fill(background_color, rect=[857,100,350,600])
 
         # put time_slot in visualization
         for time_slot in schedule.time_slots:
-            pygame.draw.rect(gameDisplay, (202, 222, 218), [time_slot.day * 100 , time_slot.hour * 100 ,100,100])
+            pygame.draw.rect(gameDisplay, timeslot_color, [time_slot.day * 100 + 75, time_slot.hour * 100 + 30,100,100])
 
-            # initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
-            myfont = pygame.font.SysFont("comicsansms", 15)
+            # initialize font
+            myfont = pygame.font.SysFont("helvetica", 18)
 
             # render text
-            label = myfont.render(time_slot.name[:3], 3, (0,0,0))
-            gameDisplay.blit(label, (time_slot.day * 100 +30 , time_slot.hour * 100 + 40))
+            label = myfont.render(time_slot.name[:6], 3, text_color)
+            gameDisplay.blit(label, (time_slot.day * 100 + 75 + 20, time_slot.hour * 100 + 30 + 40))
 
-        # initialize 5x5 grid
+        # headers font
+        headerfont = pygame.font.SysFont("helvetica", 15)
+
+        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        hours = ["09:00", "11:00", "13:00", "15:00", "17:00"]
+
+        # draw header rectangles for days and hours
+        pygame.draw.rect(gameDisplay, (189,189,189), [75, 0, 500, 30])
+        pygame.draw.rect(gameDisplay, (189,189,189), [0, 30, 75, 500])
+
+        # header text
+        for i in range (5):
+            # days header
+            label = headerfont.render(days[i], 3, text_color)
+            gameDisplay.blit(label, (92 + i* 100, 4))
+            # hours header
+            label2 = headerfont.render(hours[i], 3, text_color)
+            gameDisplay.blit(label2, (32, 31 + i*100))
+
+        # draw 5x5 grid
         for i in range(5):
             # horizontal lines
-            pygame.draw.line(gameDisplay, black, (0,i*100), (500, i*100))
+            pygame.draw.line(gameDisplay, (0,0,0), (0, i*100 + 30), (575, i*100 + 30))
 
             # vertical lines
-            pygame.draw.line(gameDisplay, black, (i * 100,0), (i * 100 ,500))
+            pygame.draw.line(gameDisplay, (0,0,0), (i * 100 + 75, 0), (i * 100 + 75, 575))
 
-        # put correct caption
+        # set correct caption
         if schedule.type == "room":
         	pygame.display.set_caption('Rooster: ' + schedule.class_room)
         else:
@@ -91,9 +68,15 @@ def visualize(schedule):
 
         pygame.display.update()
 
+# run main function in schedule_maker
+schedule = schedule_maker.main()
+
+schedule_room = schedule[0]
+schedule_student_list = schedule[1]
+
 # visualize room schedule
 visualize(schedule_room)
 
 # visualize students schedules
-for schedule_student in schedule_student_list:
-    visualize(schedule_student)
+# for schedule_student in schedule_student_list:
+#     visualize(schedule_student)
